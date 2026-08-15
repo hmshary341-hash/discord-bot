898a0fa51d8d3eb8bf763a785169522ecbb3f311
hmshary341-hash <hmshary341@gmail.com>
2026-08-15 12:40:39 +0300

    Create general.py

---
+++ cogs/general.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # أمر بينج التجريبي
+    @commands.command(name='ping')
+    async def ping(self, ctx):
+        await ctx.send('Pong! 🏓 السيرفر شغال وزي الفل.')
+
+async def setup(bot):
+    await bot.add_cog(General(bot))
