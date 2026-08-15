52cefff2ce5ac812d09760ce231de52f338af9ae
hmshary341-hash <hmshary341@gmail.com>
2026-08-15 11:53:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import os
 import discord
+from flask import Flask
+from threading import Thread
 
+# إعداد سيرفر الويب المصغر لمنع البوت من النوم
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive and running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# إعدادات ديسكورد الأساسية
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -9,6 +26,17 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'تم تسجيل الدخول بنجاح باسم: {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!ping':
+        await message.channel.send('Pong!')
+
+# تشغيل سيرفر الويب أولاً ثم تشغيل البوت
+keep_alive()
 
 token = os.getenv('TOKEN')
 client.run(token)
